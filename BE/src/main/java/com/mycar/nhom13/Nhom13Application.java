fb646525737b57f22db59d1fa68bb25d280fe93a
Phuoc Vo <voroberrt@gmail.com>
2024-05-19 20:20:41 +0700

    khong co gi

--- BE/src/main/java/com/mycar/nhom13/Nhom13Application.java
+++ BE/src/main/java/com/mycar/nhom13/Nhom13Application.java
@@ -1,15 +1,7 @@
 package com.mycar.nhom13;
 
-import com.mycar.nhom13.Entity.TaiKhoan;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Nhom13Application{

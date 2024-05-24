7314a2dfd5d2cc9677054fa12d0f91b17b6933fd
Phuoc Vo <voroberrt@gmail.com>
2024-05-24 21:26:52 +0700

    update validate dto

--- BE/src/main/java/com/mycar/nhom13/Dto/PostCarDTO.java
+++ BE/src/main/java/com/mycar/nhom13/Dto/PostCarDTO.java
@@ -1,21 +1,57 @@
 package com.mycar.nhom13.Dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class PostCarDTO {
+    
+    @NotBlank(message = "License plates cannot be empty")
+    @Size(min = 9, max = 10, message = "License plates must be between 9 and 10 characters")
     private String licensePlates;
+
+    @Size(max = 20, message = "Model must be less than or equal to 20 characters")
     private String model;
+
     private String brand;
+
+    @Min(value = 1886, message = "Year must be from 1886 onwards")
+    @Max(value = 2024, message = "Year must be 2024 or earlier")
     private int year;
+
+    
     private String color;
+
+    @DecimalMin(value = "0.0", message = "Consumption must be at least 0.0")
     private float consumption;
+
+    @Size(max = 2000, message = "Description must be less than or equal to 2000 characters")
     private String description;
+
+    @Min(value = 1, message = "Seat count must be at least 1")
     private int seat;
+
     private String type;
+
+    @Pattern(regexp = "Tu dong|So san", message = "Gear must be 'Tu dong' or 'So san'")
     private String gear;
+
+    @Pattern(regexp = "Xang|Dau|Dien", message = "Fuel must be 'Xang', 'Dau', or 'Dien'")
     private String fuel;
+
+    
     private String address;
+
     private String province;
     private String district;
+
+    @Min(value = 0, message = "Cost must be at least 0")
     private int cost;
+
+
 	public PostCarDTO() {
 		super();
 		// TODO Auto-generated constructor stub

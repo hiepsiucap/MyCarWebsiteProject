bacfd4f4b25d3ec86caef796758b4ea2ca4d86b6
Phuoc Vo <voroberrt@gmail.com>
2024-05-24 15:49:27 +0700

    update review controller

--- BE/src/main/java/com/mycar/nhom13/Entity/Review.java
+++ BE/src/main/java/com/mycar/nhom13/Entity/Review.java
@@ -3,6 +3,8 @@ package com.mycar.nhom13.Entity;
 import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -10,19 +12,26 @@ import jakarta.validation.constraints.Size;
 @Entity
 @Table(name = "Reviews")
 public class Review {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "review_seq")
-    @SequenceGenerator(name = "review_seq", sequenceName = "review_seq", allocationSize = 1)
-    @Column(name = "review_id")
-    private int reviewId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "review_seq")
+	@SequenceGenerator(name = "review_seq", sequenceName = "review_seq", allocationSize = 1)
+	@Column(name = "review_id")
+	private int reviewId;
+
+    
     @OneToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,
             CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "rental_id")
     @JsonIgnore
     private Rental rental;
-    @Pattern(regexp = "[1-5]",message = "Số đánh giá không hợp lệ.")
+    
+    @Min(value = 1, message = "Số đánh giá không hợp lệ. Phải từ 1 đến 5.")
+    @Max(value = 5, message = "Số đánh giá không hợp lệ. Phải từ 1 đến 5.")
+    @Column(name = "rate")
     private int rate;
+    
     @Size(min=1,message = "Nội dung không được để trống.")
+    @Column(name = "details")
     private String details;
     
     @Column(name = "review_date")

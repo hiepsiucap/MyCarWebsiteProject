1bc548a40d3993a2ce0fc4728f8928fb6449b347
minh6824pro <minhntp123456@gmail.com>
2024-05-25 10:50:36 +0700

    update

---
+++ BE/src/main/java/com/mycar/nhom13/Dto/StringResponse.java
@@ -0,0 +1,20 @@
+package com.mycar.nhom13.Dto;
+
+public class StringResponse {
+    private String message;
+
+    public StringResponse(){
+
+    }
+    public StringResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
